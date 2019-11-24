1985c58311be3e6e06c2084495b698e7650a022a
Ryan M <rmccauleymusic@gmail.com>
2019-11-24 18:28:41 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,7 +3,7 @@ class ChattyStudent < Student
 
   def hello
     super
-     hello
+     self.hello
     super
     "I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night?
     You didn't?! Oh man, it was so crazy! What, you don't want any spoilers?
