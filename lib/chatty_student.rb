1c42be8f9cb43ac40675dec5a28f59805b60f43b
Ryan M <rmccauleymusic@gmail.com>
2019-11-24 18:27:30 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,7 +3,7 @@ class ChattyStudent < Student
 
   def hello
     super
-     phrase =self.hello "How are you doing today?
+     phrase =hello "How are you doing today?
     I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night?
     You didn't?! Oh man, it was so crazy! What, you don't want any spoilers?
     Okay well let me just tell you who died..."
